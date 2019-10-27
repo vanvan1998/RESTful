065ca3c097bdcc0f71e9a82784dee29edcccbb4c
vanvan1998 <nguyensivan1998@gmail.com>
2019-10-27 23:30:59 +0700

    update /update

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -6,33 +6,39 @@ const passport = require('../config/passport.config');
 const User = require('../user/user.model');
 const config = require('../config/config');
 
-module.exports = (app) => {
-    app.use('/users', router);
+module.exports = app => {
+  app.use('/users', router);
 
-    router.get('/me', passport.authenticate('jwt', { session: false }), (req, res) => {
-        return res.status(200).json(req.user);
-    });
-    
-    router.post('/update', passport.authenticate('jwt', { session: false }), (req, res) => {
-        if (req.body.newPassword != req.body.confirmNewPassword) {
-            return res.status(401).json({ message: 'New password and confirm password is not match' });
+  router.get(
+    '/me',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+      return res.status(200).json(req.user);
+    }
+  );
+
+  router.post('/update', (req, res) => {
+    if (req.body.newPassword != req.body.confirmNewPassword) {
+      return res
+        .status(401)
+        .json({ message: 'New password and confirm password is not match' });
+    }
+    bcrypt.hash(req.body.newPassword, config.saltRounds, (err, hash) => {
+      var UpdateUser = {
+        $set: {
+          name: req.body.name,
+          email: req.body.email,
+          dateOfBirth: req.body.dateOfBirth,
+          sex: req.body.sex,
+          password: hash
         }
-        bcrypt.hash(req.body.newPassword, config.saltRounds, (err, hash) => {
-            var UpdateUser = {
-                $set: {
-                    name: req.body.name,
-                    email: req.body.email,
-                    dateOfBirth: req.body.dateOfBirth,
-                    sex: req.body.sex,
-                    password: hash
-                }
-            };
-            User.findByIdAndUpdate({ _id: req.body._id }, UpdateUser, (err, user) => {
-                if (err) {
-                    return res.status(500).json(err);
-                }
-                res.status(200).json({ message: 'Update user: ' + req.body.username + ' success' });
-            });
-        });
+      };
+      User.findByIdAndUpdate({ _id: req.body._id }, UpdateUser, (err, user) => {
+        if (err) {
+          return res.status(500).json(err);
+        }
+        return res.status(200).json({ message: 'Update user success' });
+      });
     });
+  });
 };
